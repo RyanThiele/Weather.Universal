2eecc62fbd4dcad8bfe66d19c0848c0972a46fd4
RyanThiele <dynamensions@live.com>
2018-08-02 09:43:07 -0400

    WPF is no longer targeting Windows 8 runtime. Possible that this is causing the error in build.

--- Weather.Universal/Weather.WPF/Services/SettingsService.vb
+++ Weather.Universal/Weather.WPF/Services/SettingsService.vb
@@ -1,6 +1,5 @@
 ﻿Imports Weather.Models
 Imports Weather.Services
-Imports Windows.Devices.Geolocation
 Imports System.Threading
 
 Namespace Services
@@ -8,16 +7,17 @@ Namespace Services
     Public Class SettingsService
         Implements ISettingsService
         Public Async Function GetCurrentLocationAsync(token As CancellationToken) As Task(Of Models.GeoCoordinate) Implements ISettingsService.GetCurrentLocationAsync
-            Dim geolocation As New Geolocator
-            Dim result As Geoposition = Await geolocation.GetGeopositionAsync.AsTask
+            Throw New NotImplementedException
+            'Dim geolocation As New Geolocator
+            'Dim result As Geoposition = Await geolocation.GetGeopositionAsync.AsTask
 
-            If result Is Nothing Then
-                Return Nothing
-            Else
-                Return New Models.GeoCoordinate With {
-                    .Latitude = CType(result.Coordinate.Latitude, Decimal),
-                    .Longitude = CType(result.Coordinate.Longitude, Decimal)}
-            End If
+            'If result Is Nothing Then
+            '    Return Nothing
+            'Else
+            '    Return New Models.GeoCoordinate With {
+            '        .Latitude = CType(result.Coordinate.Latitude, Decimal),
+            '        .Longitude = CType(result.Coordinate.Longitude, Decimal)}
+            'End If
         End Function
 
         Public Async Function GetSelectedLocationsAsync() As Task(Of IEnumerable(Of Location)) Implements ISettingsService.GetSelectedLocationsAsync
